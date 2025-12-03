ade21a75138c4cfaced006fafe44ed197ae6eb3a
satyam8540 <satyamkumar00049@gmail.com>
2025-12-03 21:44:44 +0530

    url fixed

--- server/index.js
+++ server/index.js
@@ -25,7 +25,10 @@ connectDB().then(ensureAdminUser);
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
-    credentials: true
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type','Authorization'],
+    credentials: true,
+    preflightContinue: false
   })
 );
 app.use(express.json());
